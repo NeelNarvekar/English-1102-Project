4d66dd750b4ba6beedeec05d7e7f3c22e4be6bc5
NeelNarvekar <weasel.nn@gmail.com>
2017-11-24 17:46:27 -0500

    English 1102 Project Final
    Changes

---
+++ JavaScript1.js
@@ -0,0 +1,142 @@
+contentToAdd = "'License and registration.'"
+
+function addContent(contentToAdd) {
+    document.getElementById("TEST").innerHTML = contentToAdd;
+}
+
+function showButton() {
+    document.getElementById("BUTTON").style.display = "block";
+    document.getElementById("BUT_0").disabled = true;
+}
+
+function A() {
+    contentToAdd1 = "'One of your headlights is out so I was checking everything out to make sure nothing's wrong. Hey... What's that going on in the back of the car?' Statistic: As an African-American, you are 31% more likely to be pulled over than a White person. As a Latino, you are 23% more likely to be pulled over than a White person."
+}
+
+function B() {
+    contentToAdd1 = "'Well, one of your headlights is out so I'm going to have to give you a ticket. Hey... What's that going on in the back of the car?' Statistic: As an African-American, you are 31% more likely to be pulled over than a White person. As a Latino, you are 23% more likely to be pulled over than a White person."
+}
+
+function addContent1(contentToAdd1) {
+    document.getElementById("TEST2").innerHTML = contentToAdd1
+}
+
+function showButton2() {
+    document.getElementById("BUTTON2").style.display = "block";
+    document.getElementById("BUT").disabled = true;
+    document.getElementById("BUT_1").disabled = true;
+}
+
+function C() {
+    contentToAdd2 = "'I'm going to have to ask you and the others to step out of the car so I can search it.'"
+}
+
+function D() {
+    contentToAdd2 = "'Step outside of the car and put your hands on the hood! Everyone!'"
+}
+
+function addContent2(contentToAdd2) {
+    document.getElementById("TEST3").innerHTML = contentToAdd2
+}
+
+function showButton3() {
+    document.getElementById("BUTTON3").style.display = "block";
+    document.getElementById("BUT2").disabled = true;
+    document.getElementById("BUT_2").disabled = true;
+}
+
+function E() {
+    contentToAdd3 = "Statistic: As an African-American you are 3x more likely to have your car searched than a White person. As a Latino, you are 2x more likely to have your car searched than a White person. The police officer searches your car and finds drugs..."
+}
+
+function F() {
+    contentToAdd3 = "'Get out now!' Statistic: As an African-American you are 3x more likely to have your car searched than a White person. As a Latino, you are 2x more likely to have your car searched than a White person. The police officer searches your car and finds drugs..."
+}
+
+function addContent3(contentToAdd3) {
+    document.getElementById("TEST4").innerHTML = contentToAdd3
+}
+
+function showButton4() {
+    document.getElementById("BUTTON4").style.display = "block";
+    document.getElementById("BUT3").disabled = true;
+    document.getElementById("BUT_3").disabled = true;
+}
+
+function G() {
+    contentToAdd4 = "'You're all under arrest' Statistic: As an African-American you are 3.7x more likely to be arrested for possession of marijuana than a White person. As a Latino you are 4x more likely to be arrested for possession of marijuana than a White person."
+}
+
+function H() {
+    contentToAdd4 = "'Your friends are under arrest. And you're going to have to have a conversation with me back at the station.' Statistic: As an African-American you are 3.7x more likely to be arrested for possession of marijuana than a White person. As a Latino you are 4x more likely to be arrested for possession of marijuana than a White person."
+}
+
+function addContent4(contentToAdd4) {
+    document.getElementById("TEST5").innerHTML = contentToAdd4
+}
+
+function showButton5() {
+    document.getElementById("BUTTON5").style.display = "block";
+    document.getElementById("BUT4").disabled = true;
+    document.getElementById("BUT_4").disabled = true;
+}
+
+function I() {
+    contentToAdd5 = "'You're all going to spend some time in jail for possession and use of drugs.' Choose a characteristic for yourself: Are you addicted to marijuana?"
+}
+
+function J() {
+    contentToAdd5 = "'Come on let's go. You're all going to spend some time in jail for possession and use of drugs.' Choose a characteristic for yourself: Are you addicted to marijuana?"
+}
+
+function addContent5(contentToAdd5) {
+    document.getElementById("TEST6").innerHTML = contentToAdd5
+}
+
+function showButton6() {
+    document.getElementById("BUTTON6").style.display = "block";
+    document.getElementById("BUT5").disabled = true;
+    document.getElementById("BUT_5").disabled = true;
+}
+
+function K() {
+    contentToAdd6 = "Unfortunately, the police system does not often provide aid with drug rehabilitation. You spend time in jail, get released, but soon find yourself arrested for possession of drugs once again. Statistic: Only 17% of prisoners that meet criteria for drug abuse recieve aid. 75% of people sent to prison for drug offenses are African-American or Latino."
+}
+
+function L() {
+    contentToAdd6 = "Though you are arrested for possession, fortunately you are not addicted to drugs as the police system deals poorly with drug rehabilitation. Statistic: Only 17% of prisoners that meet criteria for drug abuse recieve aid. 75% of people sent to prison for drug offenses are African-American or Latino."
+}
+
+function addContent6(contentToAdd6) {
+    document.getElementById("TEST7").innerHTML = contentToAdd6
+}
+
+function showButton7() {
+    document.getElementById("BUTTON7").style.display = "block";
+    document.getElementById("BUT6").disabled = true;
+    document.getElementById("BUT_6").disabled = true;
+}
+
+function M() {
+    contentToAdd7 = "With this offense on your background. It is difficult to find a job. You struggle without the supply of money and need desparately need some supplies. What will you do?"
+}
+
+function addContent7(contentToAdd7) {
+    document.getElementById("TEST8").innerHTML = contentToAdd7
+}
+
+function showButton8() {
+    document.getElementById("BUTTON8").style.display = "block";
+    document.getElementById("BUT7").disabled = true;
+    document.getElementById("BUT_7").disabled = true;
+}
+
+function O() {
+    contentToAdd8 = "You fall into the cycle once again."
+}
+
+function addContent8(contentToAdd8) {
+    document.getElementById("TEST9").innerHTML = contentToAdd8
+    document.getElementById("BUT8").disabled = true;
+    document.getElementById("BUT_8").disabled = true;
+}
